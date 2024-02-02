a41fc73fae7ebca0c34804b2eda180ef52a45a53
rlghks1490 <82919729+rlghks1490@users.noreply.github.com>
2024-02-03 01:24:56 +0900

    [level 0] Title: 수 조작하기 1, Time: 2.37 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,22 @@
+function solution(n, control) {
+    for (let i = 0; i < control.length; i++) {
+        switch (control[i]) {
+            case 'w':
+            n += 1;
+            break;
+            
+            case 's':
+            n -= 1;
+            break;
+            
+            case 'd':
+            n += 10;
+            break;
+            
+            default:
+            n -= 10;
+            break;
+        }
+    }
+    return n
+}
